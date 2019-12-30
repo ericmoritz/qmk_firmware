0e0615fe9aa76db4189f2c108291a77e0f83f7c8
Eric Moritz <eric@corsha.com>
2019-12-30 15:41:50 -0500

    fix: TAPPING_TERM 1000 seems good
    I havn't accidentally hit a mod combo at this term

--- keyboards/iris/keymaps/eam/config.h
+++ keyboards/iris/keymaps/eam/config.h
@@ -58,4 +58,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define STARTUP_SONG SONG(PREONIC_SOUND)
 #define STARTUP_SONG SONG(CLOSE_ENCOUNTERS_5_NOTE)
 
-#define TAPPING_TERM 500
+#define TAPPING_TERM 1000
