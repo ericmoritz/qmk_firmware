9c38655361c2096afe977aeef2c15a5d8535a304
Eric Moritz <eric@corsha.com>
2022-12-05 11:48:28 -0500

    Trying to move space and enter to pinkies and shift to thumbs
    modifiers on pinkies are annoying

--- keyboards/sofle/keymaps/eam/keymap.c
+++ keyboards/sofle/keymaps/eam/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //|-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+-------+-------|
    KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
 //|-------+-------+-------+-------+-------+-------+-------.    .-------+-------+-------+-------+-------+-------+-------|
-   KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_BTN3,     KC_MUTE, KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+   KC_SPC , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_BTN3,     KC_MUTE, KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_ENT ,
 //`-------+-------+-------+-------+-------+-------+-------     |-------+-------+-------+-------+-------+-------+-------'
-                    KC_LGUI,KC_LALT,KC_LCTL,KC_LOWR,KC_SPC,     KC_ENT ,KC_LOWR,KC_RCTL,KC_LALT,KC_RGUI
+                    KC_LGUI,KC_LALT,KC_LCTL,KC_LOWR,KC_LSFT,    KC_RSFT,KC_LOWR,KC_RCTL,KC_LALT,KC_RGUI
 //                `-------+-------+-------+-------+-------'    `-------+-------+-------+-------+-------'
 ),
 /* LOWER
